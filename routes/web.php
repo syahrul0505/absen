3b85cb385b1420ec482f0cf1fe4da6ff64e1c740
syahrul0505 <syahrulalif1234@gmail.com>
2022-07-21 00:30:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Http\Controllers\backend\DashboardController;
+use App\Http\Controllers\backend\DepartementController;
+use App\Http\Controllers\backend\HistoryLogController;
+use App\Http\Controllers\backend\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// =================================================================================================================================
+// ======================================================BACKEND ROUTE==============================================================
+// =================================================================================================================================
+    // Route::get('yakesma/admin/login', function () {
+    //     $data['page_title'] = "Login";
+    //     return view('backend.auth.login', $data);
+    // })->name('user.login');
+
+    // Route::get('/', function () {
+    //     return view('frontend.welcome');
+    // })->name('home');
+
+    Route::middleware('auth:web')->group(function () {
+
+        Route::prefix('admin/')->name('backend.')->group(function ()  {
+ 
+            // Dashboard
+            Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard.index');
+
+            // Master Data Prefix
+            Route::prefix('master-data')->group(function () {
+
+                // Master Data
+                Route::get('/', function () {
+                    $data['page_title'] = 'Master Data';
+                    $data['breadcumb'] = 'Master Data';
+                    return view('backend.master-data.index', $data);
+                })->name('master-data.index');
+
+                // Departement
+                Route::resource('departements', DepartementController::class);
+
+                // Users
+                Route::patch('change-password', [UserController::class, 'changePassword'])->name('users.change-password');
+                Route::resource('users', UserController::class)->except([
+                    'show'
+                ]);;
+            });
+            
+            // History Log
+            Route::resource('history-log', HistoryLogController::class)->except([
+                'show', 'create', 'store', 'edit', 'update'
+            ]);;
+        });
+    });
+// =================================================================================================================================
+// ==================================================END BACKEND ROUTE==============================================================
+// =================================================================================================================================
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// =================================================================================================================================
+// =====================================================FRONTEND ROUTE==============================================================
+// =================================================================================================================================
+
+
+
+
+
+
+// =================================================================================================================================
+// =====================================================END FRONTEND ROUTE==========================================================
+// =================================================================================================================================
+
